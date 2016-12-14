6a8931ae17161e11c13b35bb3d1f33c54d362bc0
Brendan Duff <brendan.duff@students.sacs.k12.in.us>
2016-12-14 08:26:43 -0500

    Brendan is number 5

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-
+#Team 2 Brendna Duff number 5
 ##### 
 # Computer Science and Software Engineering
 # PLTW AP CS Principles
@@ -380,3 +380,4 @@ def post_to_file(string, filename='tournament.txt', directory=''):
 if __name__ == '__main__':
     scores, moves, reports = main_play(modules[0:4])   
     section0, section1, section2, section3 = reports
+    
